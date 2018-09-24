e961aed1b8d19c621250ee0ca47539ce20d832a2
MefitHp <mefitDev@gmail.com>
2018-09-23 19:41:34 -0500

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Routes from './Routes'
 import { Navbar } from './components/common/Navbar/Navbar'
+import { Footer } from './components/common/Footer/Footer'
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
         <Routes />
+        <Footer />
       </div>
     )
   }
---
+++ src/components/common/Footer/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export const Footer = () => (
+    <footer>
+        <nav className="uk-navbar-container uk-navbar-transparent" uk-navbar="" >
+            <div className="uk-navbar-center">
+                <ul className="uk-navbar-nav">
+                    <li>
+                        <a target="_blank" href="https://www.facebook.com/fixterme/">
+                            <span className="uk-icon uk-margin-small-right" uk-icon="icon: facebook"></span>
+                            Facebook
+                        </a>
+                    </li>
+                    <li>
+                        <a target="_blank" href="https://www.instagram.com/fixtergeek/">
+                            <span className="uk-icon uk-margin-small-right" uk-icon="icon: instagram"></span>
+                            Instagram
+                        </a>
+                    </li>
+                    <li class="uk-nav-divider"></li>
+                    <li>
+                        <a style={{ pointerEvents: 'none', cursor: 'default' }} href="">
+                            Arango Restaurant, {new Date().getFullYear()}™
+                        </a>
+                    </li>
+
+                </ul>
+            </div>
+        </nav>
+    </footer>
+);
