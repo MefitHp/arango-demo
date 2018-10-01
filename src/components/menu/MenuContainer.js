8629c9439895f2431679ee8834154344dce2c87f
Brenda Gonzalez <bremin11.20.93@gmail.com>
2018-10-01 17:10:53 -0500

    detalles mas

--- src/components/menu/MenuContainer.js
+++ src/components/menu/MenuContainer.js
@@ -24,20 +24,20 @@ class MenuContainer extends Component {
         <div className="uk-container" >
             <h3 className="uk-text-lead " style={{marginTop:"30px"}}>Menú</h3>
             <ul uk-accordion="multiple: true">
-                <li className="uk-open">
-                    <a className="uk-accordion-title " href="#">Alimentos</a>
+                <li >
+                    <a className="uk-accordion-title bot" href="#">Alimentos</a>
                     <div className="uk-accordion-content">
                         <p className="bot">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     </div>
                 </li>
                 <li>
-                    <a className="uk-accordion-title " href="#">Bebidas</a>
+                    <a className="uk-accordion-title bot" href="#">Bebidas</a>
                     <div className="uk-accordion-content">
                         <p className="">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor reprehenderit.</p>
                     </div>
                 </li>
                 <li>
-                    <a className="uk-accordion-title " href="#">Postres</a>
+                    <a className="uk-accordion-title bot" href="#">Postres</a>
                     <div className="uk-accordion-content">
                         <p className="">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat proident.</p>
                     </div>
