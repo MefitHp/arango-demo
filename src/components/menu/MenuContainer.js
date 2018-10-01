2cf49c0d6d0a3a8bfec25be008304633fa65f999
Brenda Gonzalez <bremin11.20.93@gmail.com>
2018-10-01 17:13:19 -0500

    menu scroll

--- src/components/menu/MenuContainer.js
+++ src/components/menu/MenuContainer.js
@@ -25,19 +25,19 @@ class MenuContainer extends Component {
             <h3 className="uk-text-lead " style={{marginTop:"30px"}}>Menú</h3>
             <ul uk-accordion="multiple: true">
                 <li >
-                    <a className="uk-accordion-title bot" href="#">Alimentos</a>
+                    <a className="uk-accordion-title " href="#">Alimentos</a>
                     <div className="uk-accordion-content">
-                        <p className="bot">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     </div>
                 </li>
                 <li>
-                    <a className="uk-accordion-title bot" href="#">Bebidas</a>
+                    <a className="uk-accordion-title " href="#">Bebidas</a>
                     <div className="uk-accordion-content">
                         <p className="">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor reprehenderit.</p>
                     </div>
                 </li>
                 <li>
-                    <a className="uk-accordion-title bot" href="#">Postres</a>
+                    <a className="uk-accordion-title " href="#">Postres</a>
                     <div className="uk-accordion-content">
                         <p className="">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat proident.</p>
                     </div>
